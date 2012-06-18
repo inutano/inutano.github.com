62215e62640e6f652d98516f2c567fb2227d41a2
tazro inutano ohta <inutano@gmail.com>
2012-06-19 00:58:51 +0900

    fixed bugfool

--- home_gen.rb
+++ home_gen.rb
@@ -16,13 +16,13 @@ if __FILE__ == $0
   new_article = "<article>" + new_text_html + "</article>"
   
   home = "./index.html"
-  template =  home ? open(home).read : template_init
+  template =  File.exist?(home) ? open(home).read : template_init
   
-  nkgr = Nokogiri::HTML.new(template)
+  nkgr = Nokogiri::HTML(template)
   entries_posted = nkgr.css("article").to_html
   added = new_article + "\n" + entries_posted
   
   updated = template_init.gsub("REPLACE HERE",added)
-  FileUtils.mv(home,"#{home}.#{Time.now}") if File.exist?(home)
+  FileUtils.mv(home,"#{home}.#{Time.now.strftime("%Y%m%d%H%M%S")}") if File.exist?(home)
   open(home,"w"){|f| f.puts(updated) }
 end

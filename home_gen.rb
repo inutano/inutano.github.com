edce2083069caf967e71be9c82ece2f9b6ebb881
tazro inutano ohta <inutano@gmail.com>
2012-06-18 23:14:56 +0900

    it could be enough at least for me to work

---
+++ home_gen.rb
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+require "haml"
+require "bluecloth"
+require "nokogiri"
+require "fileutils"
+
+def template_init
+  template = open("./template.haml").read
+  Haml::Engine.new(template).render
+end
+
+if __FILE__ == $0
+  new_text = open(ARGV.first).read
+  new_text_html = BlueCloth.new(new_text).to_html
+  new_article = "<article>" + new_text_html + "</article>"
+  
+  home = "./index.html"
+  template =  home ? open(home).read : template_init
+  
+  nkgr = Nokogiri::HTML.new(template)
+  entries_posted = nkgr.css("article").to_html
+  added = new_article + "\n" + entries_posted
+  
+  updated = template_init.gsub("REPLACE HERE",added)
+  FileUtils.mv(home,"#{home}.#{Time.now}") if File.exist?(home)
+  open(home,"w"){|f| f.puts(updated) }
+end
